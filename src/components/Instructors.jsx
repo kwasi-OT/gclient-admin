1283ba0d125953d42e1c087023df9e5f9cd4c816
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2025-02-26 23:11:46 +0000

    implemented skeletal loader

--- src/components/Instructors.jsx
+++ src/components/Instructors.jsx
@@ -66,60 +66,66 @@ const Instructors = () => {
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    if (loading) return (
-        <div className="w-full text-center flex flex-col justify-center items-center">
-            <Skeleton count={10}/>
-        </div>
-    );
+    // if (loading) return (
+    //     <div className="w-full text-center flex flex-col justify-center items-center">
+    //         <Skeleton count={10}/>
+    //     </div>
+    // );
 
     
     return (
         <div className="w-full flex flex-col gap-[1rem]">
             <div className="flex justify-end mb-4">
-                <button 
-                    onClick={handleAddInstructor} 
-                    className="bg-[var(--primary-blue)] text-[white] px-4 py-2 rounded-[.3rem] flex items-center gap-[.5rem]">
-                    Add Instructor
-                    <MdOutlinePersonAddAlt size={20}/>
-                </button>
+                {loading ? 
+                    <Skeleton count={1} width={200} height={40}/> 
+                    : 
+                    <button onClick={handleAddInstructor} className="bg-[var(--primary-blue)] text-[1.2rem] text-[white] px-4 py-2 rounded-[0.3rem] flex items-center gap-[.5rem]">
+                        Add Instructor 
+                        <MdOutlinePersonAddAlt size={25}/>
+                    </button>
+                }
             </div>
-            <table className="w-full bg-[var(--input-bg)]">
-                <thead>
-                    <tr>
-                        <th className=" px-[1rem] py-[1rem]">Profile Picture</th>
-                        <th className=" px-[1rem] py-[1rem]">First Name</th>
-                        <th className=" px-[1rem] py-[1rem]">Last Name</th>
-                        <th className=" px-[1rem] py-[1rem]">Date Joined</th>
-                        <th className=" px-[1rem] py-[1rem]">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {instructor.map((instructor) => (
-                        <tr key={instructor.id}>
-                            <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] text-center">
-                                {
-                                    instructor.profile_picture ? (
-                                        <img src={instructor.profile_picture} alt={`${instructor.first_name} ${instructor.last_name}`} className="w-[3rem] h-[3rem] rounded-full" />
-                                    ) : (
-                                        <img src={AvatarPlaceholder} alt={`${instructor.first_name} ${instructor.last_name}`} className="w-[3rem] h-[3rem] rounded-full" />
-                                    )
-                                }
-                            </td>
-                            <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] ">{instructor.first_name}</td>
-                            <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] ">{instructor.last_name}</td>
-                            <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] ">{new Date(instructor.created_at).toLocaleDateString()}</td>
-                            <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] text-center ">
-                                <button onClick={() => handleView(instructor.id)} className="rounded-[0.3rem] py-[0.8rem] px-[0.8rem] mr-[0.5rem] bg-[var(--light-blue)]">
-                                    <IoEyeOutline size={25} color="var(--primary-blue)"/></button>
-                                <button onClick={() => handleEdit(instructor.id)} className="rounded-[0.3rem] py-[0.8rem] px-[0.8rem] mr-[0.5rem] bg-[var(--input-active-bg)]">
-                                    <MdOutlineEdit size={25} color="var(--primary-lemon-green)"/></button>
-                                <button onClick={() => handleDelete(instructor.id)} className="rounded-[0.3rem] py-[0.8rem] px-[0.8rem] bg-[var(--input-error-bg)]">
-                                    <ImBin size={25} color="var(--primary-red)"/></button>
-                            </td>
+            {loading ? (
+                <Skeleton count={10} height={40}/>
+            ) : (
+                <table className="w-full bg-[var(--input-bg)]">
+                    <thead>
+                        <tr>
+                            <th className=" px-[1rem] py-[1rem]">Profile Picture</th>
+                            <th className=" px-[1rem] py-[1rem]">First Name</th>
+                            <th className=" px-[1rem] py-[1rem]">Last Name</th>
+                            <th className=" px-[1rem] py-[1rem]">Date Joined</th>
+                            <th className=" px-[1rem] py-[1rem]">Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {instructor.map((instructor) => (
+                            <tr key={instructor.id}>
+                                <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] text-center">
+                                    {
+                                        instructor.profile_picture ? (
+                                            <img src={instructor.profile_picture} alt={`${instructor.first_name} ${instructor.last_name}`} className="w-[3rem] h-[3rem] rounded-full" />
+                                        ) : (
+                                            <img src={AvatarPlaceholder} alt={`${instructor.first_name} ${instructor.last_name}`} className="w-[3rem] h-[3rem] rounded-full" />
+                                        )
+                                    }
+                                </td>
+                                <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] ">{instructor.first_name}</td>
+                                <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] ">{instructor.last_name}</td>
+                                <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] ">{new Date(instructor.created_at).toLocaleDateString()}</td>
+                                <td className="bg-[var(--bg-white)] px-[1rem] py-[0.8rem] text-center ">
+                                    <button onClick={() => handleView(instructor.id)} className="rounded-[0.3rem] py-[0.8rem] px-[0.8rem] mr-[0.5rem] bg-[var(--light-blue)]">
+                                        <IoEyeOutline size={25} color="var(--primary-blue)"/></button>
+                                    <button onClick={() => handleEdit(instructor.id)} className="rounded-[0.3rem] py-[0.8rem] px-[0.8rem] mr-[0.5rem] bg-[var(--input-active-bg)]">
+                                        <MdOutlineEdit size={25} color="var(--primary-lemon-green)"/></button>
+                                    <button onClick={() => handleDelete(instructor.id)} className="rounded-[0.3rem] py-[0.8rem] px-[0.8rem] bg-[var(--input-error-bg)]">
+                                        <ImBin size={25} color="var(--primary-red)"/></button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
             {/* Pagination */}
             <div className="flex justify-center mt-4">
                 {Array.from({ length: Math.ceil(totalInstructors / instructorsPerPage) }, (_, index) => (
